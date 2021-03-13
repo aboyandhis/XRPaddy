d7a266bc2531f9499f39c6c72cb9ab8fafcbec3b
patkellydesigns <patkellydesigns@gmail.com>
2021-03-13 14:13:35 -0700

    Create camera components.

--- Source/XRPaddy/VRCharacter.cpp
+++ Source/XRPaddy/VRCharacter.cpp
@@ -8,6 +8,7 @@ AVRCharacter::AVRCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 
 }
 
